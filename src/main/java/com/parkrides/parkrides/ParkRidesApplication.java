f7d449d72fc5b7edcec34647c29ea82338d79ea3
Adem Aldemir <adem.aldemir@outlook.com>
2023-02-17 02:42:08 +0300

    Initial commit

---
+++ src/main/java/com/parkrides/parkrides/ParkRidesApplication.java
@@ -0,0 +1,13 @@
+package com.parkrides.parkrides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkRidesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkRidesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parkrides/parkrides/ParkRidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkrides.parkrides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkRidesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
