72b68afe97baeadb6af12c582187d81b75ae618c
Adem Aldemir <adem.aldemir@outlook.com>
2023-02-17 03:28:28 +0300

    Controller Layer.

---
+++ src/main/java/com/parkrides/parkrides/controller/ThemeParkRideController.java
@@ -0,0 +1,35 @@
+package com.parkrides.parkrides.controller;
+
+import com.parkrides.parkrides.entity.ThemeParkRide;
+import com.parkrides.parkrides.repository.ThemeParkRideRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class ThemeParkRideController {
+    private final ThemeParkRideRepository themeParkRideRepository;
+
+    public ThemeParkRideController(ThemeParkRideRepository themeParkRideRepository) {
+        this.themeParkRideRepository = themeParkRideRepository;
+    }
+
+    @GetMapping(value = "/ride", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<ThemeParkRide> getRides(){
+        return themeParkRideRepository.findAll();
+    }
+
+    @GetMapping(value = "/ride/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ThemeParkRide getRide(@PathVariable long id){
+        return themeParkRideRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        String.format("Invalid ride id %s", id)));
+    }
+
+    @PostMapping(value = "/ride", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ThemeParkRide createRide(@Valid @RequestBody ThemeParkRide themeParkRide){
+        return themeParkRideRepository.save(themeParkRide);
+    }
+}
