44386c4598e2fbf10ca221e56992d898a2c6723d
Adem Aldemir <adem.aldemir@outlook.com>
2023-02-17 03:28:17 +0300

    Application Starter

--- src/main/java/com/parkrides/parkrides/ParkRidesApplication.java
+++ src/main/java/com/parkrides/parkrides/ParkRidesApplication.java
@@ -1,7 +1,11 @@
 package com.parkrides.parkrides;
 
+import com.parkrides.parkrides.entity.ThemeParkRide;
+import com.parkrides.parkrides.repository.ThemeParkRideRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ParkRidesApplication {
@@ -10,4 +14,12 @@ public class ParkRidesApplication {
         SpringApplication.run(ParkRidesApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+        return (args -> {
+            repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+            repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+            repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+        });
+    }
 }
