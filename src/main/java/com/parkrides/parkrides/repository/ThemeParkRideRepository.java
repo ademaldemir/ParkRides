666d3cdd3d9b2dc45e8639764414b21a880983a9
Adem Aldemir <adem.aldemir@outlook.com>
2023-02-17 03:28:36 +0300

    Repository Layer.

---
+++ src/main/java/com/parkrides/parkrides/repository/ThemeParkRideRepository.java
@@ -0,0 +1,12 @@
+package com.parkrides.parkrides.repository;
+
+import com.parkrides.parkrides.entity.ThemeParkRide;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ThemeParkRideRepository extends JpaRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
